e8fb393d9d6ec04f1673f226c8c70dd4656b99e8
Lewizard98 <55242367+Lewizard98@users.noreply.github.com>
2019-10-31 18:37:52 +0000

    onlyAlpha2.0

--- stringOnlyalpha.cpp
+++ stringOnlyalpha.cpp
@@ -6,7 +6,7 @@ std::string fnstringOnlyalpha(std::string string1){
     std::string stringAlpha = "";
     for(int i=0;i<string1.length();i++){
         //iterate through the string, if the askii value is between a/z or A/Z then it must be alpha
-        if(string1[i] >= 'a'&& string1[i] <='z' || string1[i] >= 'A'&& string1[i] <='Z'){
+        if((string1[i] >= 'a'&& string1[i] <='z') || (string1[i] >= 'A'&& string1[i] <='Z')){
             stringAlpha += string1[i];
         }
     }
